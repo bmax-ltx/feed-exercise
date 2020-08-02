76ee6eabc85dc21aa3d20077fc552302357ffdeb
Yehuda Stiefel <ystiefel@lightricks.com>
2020-08-02 18:51:18 +0300

    changed repository context

--- app/src/main/java/com/lightricks/feedexercise/ui/feed/FeedViewModel.kt
+++ app/src/main/java/com/lightricks/feedexercise/ui/feed/FeedViewModel.kt
@@ -19,29 +19,25 @@ import java.lang.IllegalArgumentException
 /**
  * This view model manages the data for [FeedFragment].
  */
-open class FeedViewModel(application: Application) : AndroidViewModel(application) {
+open class FeedViewModel(application: Application, val feedRepository: FeedRepository) : AndroidViewModel(application) {
     private val isLoading = MutableLiveData<Boolean>()
     private val isEmpty: MediatorLiveData<Boolean>
 
     //    private val feedItems = MediatorLiveData<List<FeedItem>>()
     private val networkErrorEvent = MutableLiveData<Event<String>>()
-    private var feedRepository: FeedRepository
+//    private var feedRepository: FeedRepository
     fun getIsLoading(): LiveData<Boolean> = isLoading
     fun getIsEmpty(): LiveData<Boolean> = isEmpty
     fun getFeedItems(): LiveData<List<FeedItem>> = feedRepository.getLiveData()
     fun getNetworkErrorEvent(): LiveData<Event<String>> = networkErrorEvent
 
     init {
-        val feedApiService = initRetrofit()
-        val feedDao = FeedDatabase.getDatabase(this.getApplication()).feedDao()
-        feedRepository = FeedRepository(feedDao, feedApiService)
         isEmpty = MediatorLiveData<Boolean>().apply {
             this.addSource(getFeedItems()) {
                 this.postValue(it.isEmpty())
             }
         }
     }
-
 
     fun refresh() {
         isLoading.postValue(true)
@@ -50,9 +46,30 @@ open class FeedViewModel(application: Application) : AndroidViewModel(applicatio
             .subscribeOn(Schedulers.io())
             .subscribe(
                 { isLoading.postValue(false) },
-                { networkErrorEvent.postValue(Event<String>(it.message!!)) }
+                { networkErrorEvent.postValue(Event<String>(it?.message ?: "error"))
+                isLoading.postValue(false)}
             )
     }
+
+}
+
+/**
+ * This class creates instances of [FeedViewModel].
+ * It's not necessary to use this factory at this stage. But if we will need to inject
+ * dependencies into [FeedViewModel] in the future, then this is the place to do it.
+ */
+class FeedViewModelFactory(val app: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (!modelClass.isAssignableFrom(FeedViewModel::class.java)) {
+            throw IllegalArgumentException("factory used with a wrong class")
+        }
+        val feedApiService = initRetrofit()
+        val feedDao = FeedDatabase.getDatabase(app).feedDao()
+        val feedRepository = FeedRepository(feedDao, feedApiService)
+        @Suppress("UNCHECKED_CAST")
+        return FeedViewModel(app, feedRepository) as T
+    }
+
 
     private fun initRetrofit(): FeedApiService {
         val moshi = Moshi.Builder()
@@ -66,20 +83,5 @@ open class FeedViewModel(application: Application) : AndroidViewModel(applicatio
             .build()
             .create(FeedApiService::class.java)
         return requestInterface
-    }
-}
-
-/**
- * This class creates instances of [FeedViewModel].
- * It's not necessary to use this factory at this stage. But if we will need to inject
- * dependencies into [FeedViewModel] in the future, then this is the place to do it.
- */
-class FeedViewModelFactory(val app: Application) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (!modelClass.isAssignableFrom(FeedViewModel::class.java)) {
-            throw IllegalArgumentException("factory used with a wrong class")
-        }
-        @Suppress("UNCHECKED_CAST")
-        return FeedViewModel(app) as T
     }
 }
